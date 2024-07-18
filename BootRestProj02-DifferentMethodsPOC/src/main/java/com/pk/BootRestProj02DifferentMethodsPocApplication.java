05abc0bc30f2ca4a9b3cde8bbea2bb4bd5de7a89
# E.-PAVANKUMAR <pavanelagandula@gmail.com>
2024-07-18 07:32:44 +0530

    Merge branch 'main' of https://github.com/PAVANKUMAR1409/Spring-Boot-RestAPI.git into main

---
+++ BootRestProj02-DifferentMethodsPOC/src/main/java/com/pk/BootRestProj02DifferentMethodsPocApplication.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestProj02DifferentMethodsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestProj02DifferentMethodsPocApplication.class, args);
+	}
+
+}
