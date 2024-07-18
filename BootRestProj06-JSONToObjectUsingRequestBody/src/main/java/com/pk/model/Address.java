18c89e0de9f3bab6b00f583680da3bde29321fc4
# E.-PAVANKUMAR <pavanelagandula@gmail.com>
2024-07-18 07:35:04 +0530

    proj 06

---
+++ BootRestProj06-JSONToObjectUsingRequestBody/src/main/java/com/pk/model/Address.java
@@ -0,0 +1,13 @@
+package com.pk.model;
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+	private String houseNo;
+	private String streetName;
+	private String location;
+	private Long pincode;
+	
+}
